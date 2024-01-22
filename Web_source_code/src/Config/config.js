6d36187101097ae7f7bdb8a2b1fa4dec6dc6837f
Piush Sharma <work@shooratech.com>
2024-01-22 15:56:58 +0530

    Update config.js

--- Web_source_code/src/Config/config.js
+++ Web_source_code/src/Config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
      // Url: "http://localhost:9001/",
      // ImageUrl: "http://localhost:9001/"
-     Url: "https://coliving.waysdatalabs.com/",
+     Url: "https://coliving.waysdatalabs.com/api/",
      ImageUrl: "https://coliving.waysdatalabs.com/api/"
 };
